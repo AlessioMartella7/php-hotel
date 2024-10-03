bc63250a4418ba4fa59adbbd561e49b8e7fee7de
AlessioMartella7 <alessio.mart87@gmail.com>
2024-10-03 15:03:33 +0200

    printed hotels data on page

--- index.php
+++ index.php
@@ -47,12 +47,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Hotel</title>
+
+    <!-- bootstrap -->
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
 <body>
-    
+    <main>
+        <div class="container">
+            <?php foreach($hotels as $hotel) { ?>
 
-
+                <?= $hotel["name"];?>
+                <?= $hotel["description"];?>
+                <?= $hotel["parking"]; ?>
+                <?= $hotel["vote"]; ?>
+                <?= $hotel["distance_to_center"]; ?>
+                
+            <?php } ?>    
+        </div>
+    </main>
 </body>
 </html>
 
